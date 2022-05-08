b31ac7535d1d66f6b70032f1a53fca39f245214e
abdulrahmanalothman <abdulrahmanalothman@YAZILIM001>
2022-05-08 14:45:57 +0300

    Add project files.

---
+++ Datatables/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Datatables.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
